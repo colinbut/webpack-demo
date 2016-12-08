5480378a1c4401627939bd422a9fa71b6d5e3bb3
Colin But <colinbut@users.noreply.github.com>
2016-12-08 08:34:22 +0000

    javascript file for index.html

---
+++ app/index.js
@@ -0,0 +1,14 @@
+function component() {
+
+  var element = document.createElement('div');
+
+  /* lodash need this line to work */
+  element.innerHTML = _.map(['Hello','webpack'], function(item) {
+    return item + ' ';
+  });
+
+  return element;
+
+}
+
+document.body.appendChild(component());
